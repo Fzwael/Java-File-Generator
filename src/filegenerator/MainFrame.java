41bf6b042110d923e349eaa7febca0e889f3ee9c
Wael Fezai <fzwael@gmail.com>
2016-11-14 20:31:56 +0100

    adding compressing algorithms choices

--- src/filegenerator/MainFrame.java
+++ src/filegenerator/MainFrame.java
@@ -8,24 +8,23 @@ package filegenerator;
 import compressors.RLE;
 import helpers.RandomGenerator;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.RandomAccessFile;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author Fzwael
  */
 public class MainFrame extends javax.swing.JFrame {
+    
+    private boolean alg1 , alg2 , alg3 , alg4;
 
     /**
      * Creates new form MainFrame
      */
     public MainFrame() {
         initComponents();
+        // initialize all algorithms to false ( not choson )
+        alg1 = alg2 = alg3 = alg4 = false;
     }
 
     /**
@@ -43,6 +42,10 @@ public class MainFrame extends javax.swing.JFrame {
         nameInput = new javax.swing.JTextField();
         validatebtn = new javax.swing.JButton();
         sizeUnitInput = new javax.swing.JComboBox<>();
+        checkbtn1 = new javax.swing.JCheckBox();
+        checkbtn2 = new javax.swing.JCheckBox();
+        checkbtn3 = new javax.swing.JCheckBox();
+        checkbtn4 = new javax.swing.JCheckBox();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -82,6 +85,34 @@ public class MainFrame extends javax.swing.JFrame {
         sizeUnitInput.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "KO", "MO", "GO" }));
         sizeUnitInput.setToolTipText("");
 
+        checkbtn1.setText("RLE ");
+        checkbtn1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                checkbtn1ActionPerformed(evt);
+            }
+        });
+
+        checkbtn2.setText("ALG2");
+        checkbtn2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                checkbtn2ActionPerformed(evt);
+            }
+        });
+
+        checkbtn3.setText("ALG3");
+        checkbtn3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                checkbtn3ActionPerformed(evt);
+            }
+        });
+
+        checkbtn4.setText("ALG4");
+        checkbtn4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                checkbtn4ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -97,13 +128,22 @@ public class MainFrame extends javax.swing.JFrame {
                         .addGap(114, 114, 114)
                         .addComponent(nameInput, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(150, 150, 150)
-                        .addComponent(validatebtn))
-                    .addGroup(layout.createSequentialGroup()
                         .addGap(132, 132, 132)
                         .addComponent(sizeInput, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(60, 60, 60)
-                        .addComponent(sizeUnitInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(sizeUnitInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(150, 150, 150)
+                        .addComponent(validatebtn))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(33, 33, 33)
+                        .addComponent(checkbtn1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(checkbtn2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(checkbtn3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(checkbtn4)))
                 .addContainerGap(56, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -119,9 +159,15 @@ public class MainFrame extends javax.swing.JFrame {
                 .addComponent(nomFichiertxt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(nameInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 49, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 24, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(checkbtn1)
+                    .addComponent(checkbtn2)
+                    .addComponent(checkbtn3)
+                    .addComponent(checkbtn4))
+                .addGap(28, 28, 28)
                 .addComponent(validatebtn)
-                .addGap(45, 45, 45))
+                .addGap(19, 19, 19))
         );
 
         pack();
@@ -140,7 +186,7 @@ public class MainFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_nameInputActionPerformed
 
     private void validatebtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_validatebtnActionPerformed
-        int unit =  1024;
+       /* int unit =  1024;
         if (sizeUnitInput.getSelectedItem().toString().equals("MO"))
             unit =  1024 * 1024;
         if (sizeUnitInput.getSelectedItem().toString().equals("GO"))
@@ -149,9 +195,9 @@ public class MainFrame extends javax.swing.JFrame {
         int nbChar = size ; // 1 byte per char 
         //int lineLength = 1000; // TODO make user choose is
         //int nbLines = nbChar / (lineLength + 1); // retour a la ligne = 2
-        //int lastLinechars = nbChar - nbLines * (lineLength + 1);
+        //int lastLinechars = nbChar - nbLines * (lineLength + 1);*/
         try{
-            File file = new File(nameInput.getText());
+            /*File file = new File(nameInput.getText());
             PrintWriter writer = new PrintWriter(file, "UTF-8");
             for(int i=0 ; i< size / 1000 ; i++){
                 writer.print(RandomGenerator.generateRandomString(1000));
@@ -160,13 +206,53 @@ public class MainFrame extends javax.swing.JFrame {
             writer.close();
             System.out.println("calculated size is " + size);
             System.out.println("actual size is " + file.length());
-            System.out.println("NOW COMPRESSING FILE");
-            RLE.compress(file);
+            System.out.println("NOW COMPRESSING FILE");*/
+                if(alg1){
+                    System.out.println("ALGORITHME 1");
+                    // RLE.compress(file);
+                }
+                if(alg2){
+                    System.out.println("ALGORITHME 2");
+                    
+                }
+                if(alg3){
+                    System.out.println("ALGORITHME 3");
+                    
+                }
+                if(alg4){
+                    System.out.println("ALGORITHME 4");
+                    
+                }
+                
             }
         catch (Exception e) {
                 // do something
             }
     }//GEN-LAST:event_validatebtnActionPerformed
+
+    private void checkbtn1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_checkbtn1ActionPerformed
+        // TODO add your handling code here:
+        System.out.println("ALG1");
+        alg1 = !alg1;
+    }//GEN-LAST:event_checkbtn1ActionPerformed
+
+    private void checkbtn2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_checkbtn2ActionPerformed
+        // TODO add your handling code here:
+        System.out.println("ALG2");
+        alg2 = !alg2;
+    }//GEN-LAST:event_checkbtn2ActionPerformed
+
+    private void checkbtn3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_checkbtn3ActionPerformed
+        // TODO add your handling code here:
+        System.out.println("ALG3");
+        alg3 = !alg3;
+    }//GEN-LAST:event_checkbtn3ActionPerformed
+
+    private void checkbtn4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_checkbtn4ActionPerformed
+        // TODO add your handling code here:
+        System.out.println("ALG4");
+        alg4 = !alg4;
+    }//GEN-LAST:event_checkbtn4ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -204,6 +290,10 @@ public class MainFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JCheckBox checkbtn1;
+    private javax.swing.JCheckBox checkbtn2;
+    private javax.swing.JCheckBox checkbtn3;
+    private javax.swing.JCheckBox checkbtn4;
     private javax.swing.JTextField nameInput;
     private javax.swing.JTextField nbLingnetxt;
     private javax.swing.JTextField nomFichiertxt;
